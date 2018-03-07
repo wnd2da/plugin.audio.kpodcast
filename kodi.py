c1482fe2c1d49e3e4896b65f326e8784a61a36a7
soju6jan <cybersol@naver.com>
2018-03-07 14:03:49 +0900

    0.1.0
    First Release

---
+++ kodi.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+import os
+import urllib
+import xbmcplugin, xbmcgui, xbmcaddon
+from urlparse import parse_qs
+
+__addon__ = xbmcaddon.Addon()
+__language__  = __addon__.getLocalizedString
+sys.path.append(os.path.join( xbmc.translatePath( __addon__.getAddonInfo('path') ), 'resources', 'lib' ))
+from logic import *
+
+def Main():
+	for menu in TOP_MENU_LIST:
+		tmp = menu.split(':')
+		addDir(tmp[0], None, None, True, 'Menu', tmp[1], None, None)
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+def Menu(p):
+	type = p['param']
+	list = GetMenuList(type)
+	for item in list:
+		tmp = item.split(':')
+		title = tmp[0]
+		param = tmp[1]
+		addDir(title, None, None, True, 'ContentList', type, param, 1)
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+def ContentList(p):
+	type = p['param']
+	param = p['param2'] if 'param2' in p else None
+	pageNo = p['pageNo'] if 'pageNo' in p else None
+
+	hasMore = 'N'
+	if type == 'PODBBANG' or type == 'PODTY':
+		hasMore, data = GetContentList(type, param, pageNo)
+		for item in data:
+			title = item['title']
+			addDir(title, item['img'], None, True, 'EpisodeList', type, item['id'], '1')
+	elif type == 'ITUNES' and (param == 'Audio' or param == 'Video'):
+		data = GetItunesGenre(param)
+		for item in data:
+			addDir(item['name'], None, None, True, 'ContentList', type, item['json'], '1')
+	elif type == 'ITUNES' and (param != 'Audio' and param != 'Video'):
+		data = GetItunesProgramList(param)
+		for item in data:
+			plot = item['summary'] + '\n\n' + item['releaseDate']
+			infoLabels = {"mediatype":"music","label":item['name'] ,"title":item['name'],"plot":plot}
+			addDir(item['name'], item['img'], infoLabels, True, 'EpisodeList', type, item['id'], '1')
+	if pageNo != '1':
+		addDir('<< ' + __language__(30002).encode('utf8'), None, None, True, 'ContentList', type, param, str(int(pageNo)-1))
+	if hasMore == 'Y':
+		addDir(__language__(30003).encode('utf8') + ' >>', None, None, True, 'ContentList', type, param, str(int(pageNo)+1))	
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+def EpisodeList(p):
+	type = p['param']
+	id = p['param2']
+	pageNo = p['pageNo']
+	has_more, data = GetEpisodeList(type, id, pageNo)
+	for item in data:
+		title = item['title']
+		title = '[VIDEO] ' + item['title'] if item['video'] == 'Y' else item['title']
+		duration = item['duration'] if 'duration' in item else 0
+		infoLabels = {"mediatype":"music","label":item['title'] ,"title":item['title'],"plot":item['plot'], "duration":duration}
+		addDir(title, None, infoLabels, False, 'PlayVideo', type, item['url'], 'Y')
+	if pageNo != '1':
+		addDir('<< ' + __language__(30002).encode('utf8'), None, None, True, 'EpisodeList', type, id, str(int(pageNo)-1))
+
+	if has_more == 'Y':
+		addDir(__language__(30003).encode('utf8') + ' >>', None, None, True, 'EpisodeList', type, id, str(int(pageNo)+1))
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+	
+def PlayVideo( p ):
+	LOG('PlayVideo : %s' % p)
+	type = p['param']
+	param = p['param2']
+	isUrl = p['pageNo']
+	if isUrl == 'Y':
+		url = param
+	else:
+		url = GetURL(type, param)
+		if url is None:
+			addon_noti('Can\'t see')
+			return
+	item = xbmcgui.ListItem(path=url)
+	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
+
+
+#########################
+def addDir(title, img, infoLabels, isFolder, mode, param, param2, pageNo):
+	params = {'mode': mode, 'param':param, 'param2':param2, 'pageNo':pageNo}
+	url = '%s?%s' %(sys.argv[0], urllib.urlencode(params))
+	listitem = xbmcgui.ListItem(title, thumbnailImage=img)
+	if infoLabels: listitem.setInfo(type="Video", infoLabels=infoLabels)
+	if not isFolder: listitem.setProperty('IsPlayable', 'true')
+	xbmcplugin.addDirectoryItem(int(sys.argv[1]), url, listitem, isFolder)
+
+def addon_noti(sting):
+	try:
+		dialog = xbmcgui.Dialog()
+		dialog.notification(__addon__.getAddonInfo('name'), sting)
+	except:
+		LOG('addonException: addon_noti')
+
+def get_params():
+	p = parse_qs(sys.argv[2][1:])
+	for i in p.keys():
+		p[i] = p[i][0]
+	return p
+
+
+params = get_params()
+try:
+	mode = params['mode']
+except:
+	mode = None
+if mode == None: Main()
+elif mode == 'Menu': Menu(params)
+elif mode == 'ContentList': ContentList(params)
+elif mode == 'EpisodeList': EpisodeList(params)
+elif mode == 'PlayVideo': PlayVideo(params)
+elif mode == 'Test': Test(params)
+#else: LOG('NOT FUNCTION!!')
---
+++ resources/lib/logic.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+def LOG_PLATFORM(str):
+	"""
+	Log(str)
+	"""
+	import xbmc, xbmcaddon
+	try:
+		xbmc.log("[%s-%s]: %s" %(__addon__.getAddonInfo('id'), xbmcaddon.Addon().getAddonInfo('version'), str), level=xbmc.LOGNOTICE)
+		log_message = str.encode('utf-8', 'ignore')
+	except:
+		log_message = 'SC Exception'
+	xbmc.log("[%s-%s]: %s" %(xbmcaddon.Addon().getAddonInfo('id'), xbmcaddon.Addon().getAddonInfo('version'), log_message), level=xbmc.LOGNOTICE)
+
+
+from HTMLParser import HTMLParser
+
+def LOG(str): 
+	return LOG_PLATFORM(str)
+
+def ChangeHTMLChar(str):
+	return HTMLParser().unescape(str.decode('utf8'))
+
+TOP_MENU_LIST = ['팟빵:PODBBANG', '팟티:PODTY', 'iTunes Podcasts:ITUNES']
+
+def GetMenuList(type):
+	isPlay = False
+	ret = None
+	if type == 'PODBBANG':
+		ret = MENU_PODBBANG
+	elif type == 'PODTY':
+		ret = MENU_PODTY
+	elif type == 'ITUNES':
+		ret = MENU_ITUNES
+	return ret
+
+def GetContentList(type, param, pageNo):
+	if type == 'PODBBANG':	return GetPodbbangProgramList(param, pageNo)
+	elif type == 'PODTY':	return GetPodtyProgramList(param, pageNo)
+	elif type == 'EVERYON':	return GetEveryonChannelList(param, pageNo)
+
+def GetEpisodeList(type, id, page):
+	if type == 'PODBBANG':
+		return GetPodbbangEpisodeList(id, page)
+	elif type == 'PODTY':
+		return GetPodtyEpisodeList(id, page)
+	elif type == 'ITUNES':
+		return GetItunesEpisodeList(id, page)
+
+def GetURL(type, param):
+	if type == 'RADIO':
+		return GetRadioURL(param)
+	elif type == 'EVERYON':
+		return GetEveryonURL(param)
+	#elif type == 'SBS': return GetSBSURL(param)
+
+
+
+###############################################################################
+import urllib, urllib2, cookielib
+import json
+import re
+from logic import *
+
+MENU_PODBBANG = [
+	'종합순위:',
+	'시사 및 정치:0',
+	'코미디:7',
+	'도서:1',
+	'영화:3',
+	'경제:4',
+	'어학:5',
+	'교육 및 기술:6',
+	'스포츠:8',
+	'음악:9',
+	'여행:21',
+	'건강 및 의학:10',
+	'문화 및 예술:19',
+	'취미:11',
+	'유아동:18',
+	'정부 및 기관:13',
+	'게임:16',
+	'종교:12',
+	'지역:20',
+	'해외 팟캐스트:15' ]
+MENU_PODTY = [
+	'차트:',
+	'뉴스&정치:10',
+	'IT&기술:15',
+	'코미디:3',
+	'음악:9',
+	'영화&음악:16',
+	'교육&외국어:4',
+	'게임&취미:5',
+	'스포츠:14',
+	'예술:1',
+	'시사:6',
+	'건강:7',
+	'육아&가정:8',
+	'종교:11',
+	'과학&의학:12',
+	'사회&문화:13'
+	]
+
+
+PODTY_URL = 'https://www.podty.me'
+PODBBANG_URL = 'http://www.podbbang.com'
+
+def GetPodbbangProgramList(category, pageNo='1'):
+	url = PODBBANG_URL + '/ranking?kind=daily' if category is None or category == 'None' else PODBBANG_URL + '/ranking/category?kind=daily&cate=' + category
+	url = url + '&start=' + str(((int(pageNo)-1)*100)+1)
+	hasMore = 'Y' if category is None or category == 'None' else 'N'
+	LOG('URL: %s' % url)
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	#cate dummy id title dummy summary
+	#regax = 'cate\"\>.*\"\>(.*?)\<(.*\s*){4}.*\/ch\/(.*?)\".*\"\>(.*?)\<(.*\s*){3}.*title\=\"(.*?)\<'
+	#regax = 'cate\"\>.*\"\>(.*?)\<.*\s*.*\s*.*\s*.*\s*.*\/ch\/(.*?)\".*\"\>(.*?)\<.*\s*.*title.*\s*.*title.*\s*.*title\=\"(.*?)'
+	regax = 'cate\"\>.*\"\>(.*?)\<(.*\s*){4}.*\/ch\/(.*?)\".*\"\>(.*?)\<(.*\s*){3}.*title\=\"(.*?)\<.*'
+	r = re.compile(regax)
+	m = r.findall(data)
+	#LOG('GetPodbbangProgramList %s' % m)
+	ret = []
+	for item in m:
+		info = {}
+		info['cate'] = item[0]
+		info['id'] = item[2]
+		info['title'] = ChangeHTMLChar(item[3])
+		if info['title'] == '':
+			info['title'] = '19+'
+		info['summary'] = ChangeHTMLChar(item[5])
+		info['img'] = 'http://img.podbbang.com/img/pb_m/thumb/x88/%s.png' % info['id']
+		ret.append(info)
+	return hasMore, ret
+
+def GetPodtyProgramList(category, pageNo='1'):
+	url = PODTY_URL + '/chart/podty/daily' if category is None else PODTY_URL + '/chart/daily?catId=' + category
+	LOG('>>>>> GetPodtyProgramList %s' % url)
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	regax = 'ranking\">(.*?)<.*\s*.*\s*.*\/cast\/(.*?)\">.*\"\s*(.*?)\"\s*.*alt=\"(.*?)\".*\s*.*\s*.*\">(.*?)\<.*\s*.*\"\>(.*?)\<'
+	p = re.compile(regax)
+	m = p.findall(data)
+	ret = []
+	for item in m:
+		info = {}
+		info['cate'] = item[5]
+		info['id'] = item[1]
+		info['title'] = ChangeHTMLChar(item[3])
+		info['summary'] = ChangeHTMLChar(item[4])
+		info['img'] = item[2]
+		info['index'] = item[0]
+		ret.append(info)
+	
+	return 'N', ret
+
+
+def GetPodtyEpisodeList(id, page):
+	url = ('https://www.podty.me/cast/%s/episodes?page=%s&dir=desc' % (id, page))
+	LOG('GetPodtyEpisodeList URL: %s' % url)
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	regax = 'class=\"btnNext\"'
+	has_more = 'Y' if data.find(regax) != -1 else 'N'
+	regax = 'uri\=\"(.*?)\".*type\=\"(.*?)\".*name\=\"(.*?)\"(.*\s*){3}.*date\"\>(.*?)\<(.*\s*){4}(.*?)\s'
+	r = re.compile(regax)
+	m = r.findall(data)
+	ret = []
+	for item in m:
+		info = {}
+		info['title'] = ChangeHTMLChar(item[2])
+		info['video'] = 'Y' if item[1] == 'video/mp4' else 'N'
+		info['duration'] = None
+		if len(item) > 5:
+			tmp = item[6].split(':')
+			if len(tmp) == 3:
+				info['duration'] = int(tmp[2]) + int(tmp[1]) * 60 + int(tmp[0])*3600
+		info['plot'] = ChangeHTMLChar(item[2] + '\n' + item[4])
+		info['url'] = item[0]
+		ret.append(info)
+	return has_more, ret
+
+def GetPodbbangEpisodeList(id, page):
+	url = ('http://app-api4.podbbang.com/channel?channel=%s&order=desc&count=30&page=%s' % (id, page))
+	LOG('GetPodbbangEpisodeList URL: %s' % url)
+	request = urllib2.Request(url)
+	request.add_header('Podbbang', 'os=iOS&osver=4.4.2&ver=4.35.271&device=MQAG2KH/A&device_id=359090030208175&id=4349893&auth_code=cdc986a2d1184895178e8129231a39cc6c7c31ab&nick=&is_login=N&is_adult=N')
+	request.add_header('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; Nexus 6 Build/KOT49H)')
+	response = urllib2.urlopen(request)
+	data = json.load(response, encoding='utf8')
+	has_more = 'Y' if int(data['data']['total_count']) > int(page)*30 else 'N'
+	ret = []
+	for item in data['list']:
+		info = {}
+		info['url'] = item['file_url']
+		info['title'] = ChangeHTMLChar(item['title'].encode('utf8'))
+		info['plot'] = ChangeHTMLChar(item['summary'].encode('utf8')) +  '\n' + item['date']
+		info['duration'] = item['duration']
+		info['video'] = 'N' if item['type'] == 'audio' else 'Y'
+		#info['date'] = item['date']
+		ret.append(info)
+	return has_more, ret
+
+###############################################################################
+import urllib, urllib2
+import json
+import re
+from logic import *
+
+MENU_ITUNES = [	'Audio:Audio', 'Video:Video']
+GENRE_LIST_URL = 'http://itunes.apple.com/WebObjects/MZStoreServices.woa/ws/genres?id=26'
+STORE_ID = '143466'  # Korea
+
+def GetItunesGenre(type, includeSubgenre=False):
+	request = urllib2.Request(GENRE_LIST_URL)
+	request.add_header('X-Apple-Store-Front', STORE_ID)
+	response = urllib2.urlopen(request)
+	data = json.load(response, encoding='utf8')
+	data = data['26']
+	list = []
+	info = {}
+	info['name'] = 'TOP' #data['name']
+	info['json'] = data['rssUrls']['topAudioPodcasts'] if type == 'Audio' else data['rssUrls']['topVideoPodcasts']
+	list.append(info)
+	keys = data['subgenres'].keys()
+	for i in range(0, len(keys)):
+		genre = data['subgenres'][keys[i]]
+		info = {}
+		info['name'] = genre['name']
+		info['json'] = genre['rssUrls']['topAudioPodcasts'] if type == 'Audio' else genre['rssUrls']['topVideoPodcasts']
+		list.append(info)
+		if includeSubgenre and 'subgenres' in genre:
+			LOG('SUBGENRE %s' % genre['subgenres'])
+			sub_keys = genre['subgenres'].keys()
+			LOG('SUBGENRE sub_keys %s' % sub_keys)
+			for j in range(0, len(sub_keys)):
+			#for sub_genre in genre['subgenres']:
+				sub_genre = genre['subgenres'][sub_keys[j]]
+				LOG('XXXXXsub_genre %s ' % sub_genre)
+				info = {}
+				info['name'] = genre['name'] + ' - ' + sub_genre['name']
+				info['json'] = sub_genre['rssUrls']['topAudioPodcasts'] if type == 'Audio' else sub_genre['rssUrls']['topVideoPodcasts']
+				list.append(info)
+		
+	return list
+
+
+def GetItunesProgramList(url):
+	url = url.replace('/json', '/limit=50/json')
+	LOG('GetItunesProgramList URL:%s' % url)
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = json.load(response, encoding='utf8')
+	LOG('DATA %s' % data)
+
+
+	data = data['feed']
+	list = []
+	for item in data['entry']:
+		info = {}
+		info['name'] = item['title']['label']
+		count = len(item['im:image'])
+		info['img'] = item['im:image'][count-1]['label']
+		info['summary'] = ' ' if 'summary' not in item else item['summary']['label']
+		info['id'] = item['id']['attributes']['im:id']
+		info['artist'] = item['im:artist']['label']
+		info['releaseDate'] = '' if 'releaseDate' not in item else item['im:releaseDate']['label'].split('T')[0]
+		list.append(info)
+		#LOG('INFO %s' % info)
+	return list
+
+def GetItunesEpisodeList(id, pageNo):
+	url = 'http://itunes.apple.com/lookup?id=' + id
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = json.load(response, encoding='utf8')
+	feedurl = data['results'][0]['feedUrl']
+	url = data['results'][0]['trackViewUrl']
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+	data = response.read()
+	regax = 'title=\"(.*?)\".*video-preview-url=\"(.*?)\".*'
+	r = re.compile(regax)
+	m = r.findall(data)
+	list = []
+	if len(m) > 0:
+		for item in m:
+			info = {}
+			info['url'] = item[1]
+			info['title'] = ChangeHTMLChar(item[0])
+			#info['duration'] = item[2][:4]
+			info['video'] = 'Y'
+			info['plot'] = info['title']
+			list.append(info)
+	else:
+		regax = 'audio-preview-url=\"(.*?)\".*title=\"(.*?)\".*'
+		r = re.compile(regax)
+		m = r.findall(data)
+		for item in m:
+			info = {}
+			info['url'] = item[0]
+			info['title'] = ChangeHTMLChar(item[1])
+			#info['duration'] = item[2][:4]
+			info['video'] = 'N'
+			info['plot'] = info['title']
+			list.append(info)
+	return 'N', list
